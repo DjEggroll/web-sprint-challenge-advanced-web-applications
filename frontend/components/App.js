28011f1839cbe2b2b6c3eea8eafea948f52e9624
Jiahui Feng <feng_jeffrey@yahoo.com>
2022-06-29 15:28:06 -0400

    Login and Logout functional

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,6 +6,9 @@ import Message from './Message'
 import ArticleForm from './ArticleForm'
 import Spinner from './Spinner'
 
+import axios from 'axios';
+import Api from './../axios/index';
+
 const articlesUrl = 'http://localhost:9000/api/articles'
 const loginUrl = 'http://localhost:9000/api/login'
 
@@ -18,8 +21,8 @@ export default function App() {
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => { navigate('/') }
+  const redirectToArticles = () => { navigate('/articles') }
 
   const logout = () => {
     // ✨ implement
@@ -27,6 +30,9 @@ export default function App() {
     // and a message saying "Goodbye!" should be set in its proper state.
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    localStorage.removeItem('token');
+    setMessage('Goodbye!');
+    redirectToLogin();
   }
 
   const login = ({ username, password }) => {
@@ -36,6 +42,19 @@ export default function App() {
     // On success, we should set the token to local storage in a 'token' key,
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
+    setMessage('');
+    setSpinnerOn(true);
+    axios.post("http://localhost:9000/api/login", {username, password})
+    .then(res => {
+      localStorage.setItem('token', res.data.token);
+      console.log(res);
+      setMessage(res.data.message);
+      redirectToArticles();
+      setSpinnerOn(false);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   const getArticles = () => {
@@ -47,6 +66,19 @@ export default function App() {
     // If something goes wrong, check the status of the response:
     // if it's a 401 the token might have gone bad, and we should redirect to login.
     // Don't forget to turn off the spinner!
+    setMessage('');
+    setSpinnerOn(true);
+    Api().get('/articles')
+      .then(res => {
+        console.log(res);
+        setSpinnerOn(false);
+      })
+      .catch(err => {
+        console.log(err);
+        redirectToLogin();
+        setSpinnerOn(false);
+      })
+    
   }
 
   const postArticle = article => {
@@ -68,8 +100,8 @@ export default function App() {
   return (
     // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
     <>
-      <Spinner />
-      <Message />
+      <Spinner on={spinnerOn}/>
+      <Message message={message}/>
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
@@ -78,11 +110,11 @@ export default function App() {
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm login={login}/>} />
           <Route path="articles" element={
             <>
               <ArticleForm />
-              <Articles />
+              <Articles articles={articles} getArticles={getArticles}/>
             </>
           } />
         </Routes>
--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -5,12 +5,17 @@ import PT from 'prop-types'
 export default function Articles(props) {
   // ✨ where are my props? Destructure them here
 
+  const { articles, getArticles } = props
+
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
 
   useEffect(() => {
     // ✨ grab the articles here, on first render only
-  })
+    if(!articles){
+      getArticles()
+    }
+  }, [])
 
   return (
     // ✨ fix the JSX: replace `Function.prototype` with actual functions
